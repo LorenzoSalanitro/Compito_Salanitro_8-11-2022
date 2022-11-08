78d42434e2df84b775f807e348995589a308b49a
lai2-01 <lai2-01@itismeucci.com>
2022-11-08 08:30:49 +0100

    primo commit

---
+++ server_compito/src/main/java/it/fi/meucci/Messaggio.java
@@ -0,0 +1,30 @@
+package it.fi.meucci;
+
+import java.util.ArrayList;
+
+public class Messaggio 
+{
+    ArrayList <Biglietti> Lista_biglietti = new ArrayList<>();
+
+    public Messaggio(ArrayList<Biglietti> lista_biglietti) 
+    {
+        Lista_biglietti = lista_biglietti;
+    }
+
+    public Messaggio() {}
+
+    public ArrayList<Biglietti> getLista_biglietti() {
+        return Lista_biglietti;
+    }
+
+    public void setLista_biglietti(ArrayList<Biglietti> lista_biglietti) {
+        Lista_biglietti = lista_biglietti;
+    }
+
+    @Override
+    public String toString() {
+        return "Messaggio [Lista_biglietti=" + Lista_biglietti + "]";
+    }
+
+    
+}
---
+++ server_compito/src/main/java/it/fi/meucci/ServerThread.java
@@ -0,0 +1,68 @@
+package it.fi.meucci;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerThread extends Thread
+{
+    ServerSocket server = null;
+    Socket client = null;
+    String stringRecived = null;
+    String stringModified = null;
+    BufferedReader inputFromClient;
+    DataOutputStream outputToClient;
+
+    public ServerThread (Socket socket)
+    {
+        this.client = socket;
+    }
+
+    public void run()
+    {
+        try
+        {
+            comunicate();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public void comunicate() throws Exception
+    {
+        inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        outputToClient = new DataOutputStream(client.getOutputStream());
+
+        for(;;)
+        {
+            stringRecived = inputFromClient.readLine();
+            if(stringRecived == null || stringRecived.equals("fine"))
+            {
+                outputToClient.writeBytes(stringRecived + "server closing" + '\n');
+                System.out.println("Echo on server closing: " + stringRecived);
+                break;
+            }
+            else if(stringRecived.equals("CLOSE"))
+            {
+                MultiServer.spegni();
+                server.close();
+                break;
+            }
+            else
+            {
+                outputToClient.writeBytes("received: " + stringRecived + '\n');
+                System.out.println("Echo on server: " + stringRecived);
+            }
+        }
+
+        outputToClient.close();
+        inputFromClient.close();
+
+        System.out.println("closing socket" + client);
+        client.close();
+    }
+}
